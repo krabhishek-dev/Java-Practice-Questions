665678e4a37a1d8f94dfabc8b2afb1e115543da0
Abhishek Kumar <abhibth977@gmail.com>
2026-08-16 23:57:54 +0530

    updating codes....

--- Conditionals/CheckDivisibleByNumber.java
+++ Conditionals/CheckDivisibleByNumber.java
@@ -2,10 +2,10 @@ class CheckDivisibleByNumber {
     public static void main(String[] args) {
         int number = 15;
 
-        if (number % 5 == 0 ) {
-            System.out.println(number + " is divisible by 5.");
+        if (number % 5 == 0 && number % 3 == 0) {
+            System.out.println(number + " is divisible by both 5 and 3.");
         } else {
-            System.out.println(number + " is not divisible by 5.");
+            System.out.println(number + " is not divisible by both 5 and 3.");
         }
     }
 }
