8e1738d94474db6531200e00fbad94e19c709cf7
Abhishek Kumar <abhibth977@gmail.com>
2026-08-16 23:46:30 +0530

    updating codes....

---
+++ Conditionals/CheckDivisibleByNumber.java
@@ -0,0 +1,11 @@
+class CheckDivisibleByNumber {
+    public static void main(String[] args) {
+        int number = 15;
+
+        if (number % 5 == 0 ) {
+            System.out.println(number + " is divisible by 5.");
+        } else {
+            System.out.println(number + " is not divisible by 5.");
+        }
+    }
+}
