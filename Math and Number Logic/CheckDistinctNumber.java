aa7ddf7c155882fe5c79e1eeee4bebcdaa6c8609
Abhishek Kumar <abhibth977@gmail.com>
2026-08-26 00:35:53 +0530

    updating....

---
+++ Math and Number Logic/CheckDistinctNumber.java
@@ -0,0 +1,13 @@
+// Take a 3-digit number and check if all digits are distinct.
+import java.util.Scanner;
+
+public class CheckDistinctNumber {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter 3-digit number: ");
+        int number = scanner.nextInt();
+
+        
+    }
+}
