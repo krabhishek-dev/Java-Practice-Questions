6c5ef01ce4042b10ee1850a77410ee504328c47c
Abhishek Kumar <abhibth977@gmail.com>
2026-08-26 22:47:01 +0530

    updating....

--- Math and Number Logic/CheckDistinctNumber.java
+++ Math and Number Logic/CheckDistinctNumber.java
@@ -8,6 +8,16 @@ public class CheckDistinctNumber {
         System.out.print("Enter 3-digit number: ");
         int number = scanner.nextInt();
 
-        
+        int digit1 = number / 100;
+        int digit2 = (number / 100) % 10;
+        int digit3 = number % 10;
+
+        if(digit1 != digit2 && digit1 != digit3 && digit2 != digit3 ){
+            System.out.println("All digits are distinct");
+        } else {
+            System.out.println("All digits are not distinct");
+        }
+
+        scanner.close();
     }
 }
