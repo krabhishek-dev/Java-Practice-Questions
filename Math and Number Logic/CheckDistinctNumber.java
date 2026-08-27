2a15f1882b8c6a8efb94b8730c4c838c8c29b326
Abhishek Kumar <abhibth977@gmail.com>
2026-08-27 23:18:39 +0530

    updating....

--- Math and Number Logic/CheckDistinctNumber.java
+++ Math and Number Logic/CheckDistinctNumber.java
@@ -9,7 +9,7 @@ public class CheckDistinctNumber {
         int number = scanner.nextInt();
 
         int digit1 = number / 100;
-        int digit2 = (number / 100) % 10;
+        int digit2 = (number / 10) % 10;
         int digit3 = number % 10;
 
         if(digit1 != digit2 && digit1 != digit3 && digit2 != digit3 ){
