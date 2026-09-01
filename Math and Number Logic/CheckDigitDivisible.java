a2374f6a73bcb48e162f1d7df38428607119cc68
Abhishek Kumar <abhibth977@gmail.com>
2026-09-01 23:50:48 +0530

    updating....

--- Math and Number Logic/CheckDigitDivisible.java
+++ Math and Number Logic/CheckDigitDivisible.java
@@ -1,1 +1,12 @@
 // Check if a number is a multiple of 7 or ends with 7
+public class CheckDigitDivisible {
+    public static void main(String[] args){
+        int number = 7790;
+
+        if(number % 7 == 0){
+            System.out.println("Number is a multiple of 7.");
+        }else {
+            System.out.println("Number is not multiple of 7.");
+        }
+    }
+}
