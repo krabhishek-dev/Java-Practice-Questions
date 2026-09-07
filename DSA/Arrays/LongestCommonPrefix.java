6242afa46adfb2cbc776999ebb280ced8d5189a7
Abhishek Kumar <abhibth977@gmail.com>
2026-09-07 23:59:07 +0530

    Solving DSA Problems

---
+++ DSA/Arrays/LongestCommonPrefix.java
@@ -0,0 +1,18 @@
+class LongestCommonPrefix {
+
+  
+  // Approach 1 — Vertical Scanning
+  public String longestCommonPrefix(String[] strs) {
+    for (int i = 0; i < strs[0].length(); i++) {
+      char current = strs[0].charAt(i);
+
+      for (int j = 1; j < strs.length; j++) {
+        if (i >= strs[j].length() || strs[j].charAt(i) != current) {
+          return strs[0].substring(0, i);
+        }
+      }
+    }
+
+    return strs[0];
+  }
+}
