bfe0ce1707bc79410dcb06ce7dc93935cacb3ae7
Abhishek Kumar <abhibth977@gmail.com>
2026-09-09 23:09:40 +0530

    updating...

--- Logical Operators Compound Statements/Time.java
+++ Logical Operators Compound Statements/Time.java
@@ -1,1 +1,13 @@
 // Take 24-hour time (hours and minutes) and print whether it is AM or PM.
+public class Time {
+    public static void main(String[] args){
+        int hours = 14;
+        int minutes = 30;
+
+        if(hours < 12){
+            System.out.println("AM");
+        } else {
+            System.out.println("PM");
+        }
+    }
+}
