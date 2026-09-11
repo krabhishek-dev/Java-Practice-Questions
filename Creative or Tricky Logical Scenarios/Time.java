21aea39ac57bea0c1d5e3e0eb993d7b2041b6794
Abhishek Kumar <abhibth977@gmail.com>
2026-09-11 23:38:43 +0530

    updating...

--- Creative or Tricky Logical Scenarios/Time.java
+++ Creative or Tricky Logical Scenarios/Time.java
@@ -2,19 +2,25 @@
 import java.util.Scanner;
 
 public class Time {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter hours and minutes:");
         int hours = sc.nextInt();
         int minutes = sc.nextInt();
-        
-        
-        if(){
-            System.out.println("");
-        } else {
-            System.out.println("");
+
+        hours = hours % 12;
+
+        double hourAngle = (hours * 30) + (minutes * 0.5);
+        double minuteAngle = minutes * 6;
+
+        double angle = Math.abs(hourAngle - minuteAngle);
+
+        if (angle > 180) {
+            angle = 360 - angle;
         }
+
+        System.out.println("Smaller angle: " + angle + " degrees");
 
         sc.close();
     }
